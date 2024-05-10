5c0b994f76a29d3802943f113af7faa1043bed34
Royal Finch <Royal2791@gmail.com>
2024-05-10 19:16:43 -0400

    feat: :lipstick: add styling for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 export default function App() {
   return (
     <main className="main">
-      <img src="/images/logo.svg" alt="Base Apparel Logo" className="logo" />
+      <div className="logo">
+        <img
+          src="/images/logo.svg"
+          alt="Base Apparel Logo"
+          className="logo__img"
+        />
+      </div>
       <picture>
         <source
           srcSet="/images/hero-desktop.jpg"
-          media="(min-width: 700px"
+          media="(min-width: 700px)"
           className="hero-desktop"
         />
         <img
@@ -14,23 +20,33 @@ export default function App() {
           className="hero-mobile"
         />
       </picture>
-      <h1 className="title">We&apos;re Coming Soon</h1>
-      <p className="subtext">
-        Hello fellow shoppers! We&apos;re currently building our new fashion
-        store. Add your email below to stay up-to-date with announcements and
-        our launch deals.
-      </p>
-      <form action="" noValidate>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          aria-label="Email Address"
-        />
-        <button aria-label="submit">
-          <img src="/images/icon-arrow.svg" alt="" />
-        </button>
-      </form>
+      <div className="content">
+        <h1 className="title">
+          <span className="title-accent">We&apos;re</span> Coming Soon
+        </h1>
+        <p className="subtext">
+          Hello fellow shoppers! We&apos;re currently building our new fashion
+          store. Add your email below to stay up-to-date with announcements and
+          our launch deals.
+        </p>
+        <form action="" noValidate className="form">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            aria-label="Email Address"
+            className="form__input"
+          />
+          <button aria-label="submit" className="form__btn-submit">
+            <img
+              src="/images/icon-arrow.svg"
+              alt=""
+              className="form__btn-submit__img"
+            />
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
